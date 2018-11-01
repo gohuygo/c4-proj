227e5f4b96369da8afdbb76b3cde2ca4433068b8
Huy <huy@gohuygo.com>
2018-10-31 20:18:40 -0700

    set string method

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ReadString from "./ReadString";
+import SetString from "./SetString";
 
 class App extends Component {
   state = { loading: true, drizzleState: null };
@@ -32,6 +33,10 @@ class App extends Component {
     return (
       <div className="App">
         <ReadString
+          drizzle={this.props.drizzle}
+          drizzleState={this.state.drizzleState}
+        />
+        <SetString
           drizzle={this.props.drizzle}
           drizzleState={this.state.drizzleState}
         />
