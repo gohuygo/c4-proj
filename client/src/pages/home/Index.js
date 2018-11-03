09f138ff2df3ba2a49721667520e96caadf3ea90
gohuygo <huy@gohuygo.com>
2018-11-03 14:18:19 -0700

    Some landing page design

--- client/src/pages/home/Index.js
+++ client/src/pages/home/Index.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Segment, Card } from 'semantic-ui-react';
 import styled from 'styled-components'
 
 // TODO: Remove when no longer needed for reference
@@ -34,18 +35,46 @@ class HomeIndex extends Component {
 
   render() {
     return(
-      <BlueBackgroundContainer>
-        <div class="ui vertical masthead center aligned segment">
-          <div class="ui text container">
-            <h1 class="ui inverted header">
-              Imagine-a-Company
-            </h1>
-            <h2>
-              Do whatever you want when you want to.
-            </h2>
+      <Fragment>
+        <BlueBackgroundContainer>
+          <div class="ui vertical masthead center aligned segment">
+            <div class="text">
+              <h1 class="ui inverted header">
+                Imagine-a-Company
+                <br/>
+                Do whatever you want when you want to.
+              </h1>
+            </div>
           </div>
-        </div>
-      </BlueBackgroundContainer>
+        </BlueBackgroundContainer>
+
+        <Segment vertical>
+          <Card.Group centered>
+            <Card
+              image='https://building-microgrid.lbl.gov/sites/default/files/styles/image_main/public/Berkeley%20Pic-small.png?itok=RFa6QH9B'
+              header='Berkeley, CA'
+              meta='Municipal Bond'
+              extra={<p>This belongs to the city of Berkeley</p>}
+            />
+
+            <Card
+              image='https://www.charlottefive.com/wp-content/uploads/2018/02/skyline-charlotte-nc-charlottefive-696x463.jpg'
+              header='Charlotte, NC'
+              meta='Municipal Bond'
+              extra={<p>This belongs to the city of Berkeley</p>}
+            />
+
+            <Card
+              image='https://www.omm.com/~/media/images/site/locations/san_francisco_780x520px.ashx'
+              header='San Francisco, CA'
+              meta='Municipal Bond'
+              extra={<p>This belongs to the city of Berkeley</p>}
+            />
+          </Card.Group>
+        </Segment>
+
+
+      </Fragment>
     )
   }
 }
