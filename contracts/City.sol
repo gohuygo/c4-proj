cf41fa7114dab6cf7a6601a5510c5aa23ee0ba42
Sreenivasulu Lekkala <slekkala@Sreenivasulus-MacBook-Air.local>
2018-11-03 15:57:59 -0700

    Adding City Contract and migration file

---
+++ contracts/City.sol
@@ -0,0 +1,52 @@
+pragma solidity 0.4.24;
+
+
+contract City {
+
+    struct Bond {
+        string name;
+        string startDate;
+        string orderDeadline;
+        uint   totalAmountFiat;
+        uint   totalTokensSupply;
+        string tokenSymbol;
+        address bondContractERC20Address;
+        string creditRating;
+        uint   couponRate;
+        uint   faceValue;
+        uint   marketValue;
+        uint   tTMInMonths;
+        string status;
+        string typeOfBond;
+    }
+
+    struct CityStruct {
+        address owner;
+        string state;
+        string county;
+        string city;
+        string issuer;
+        //Bond[] bonds;
+        uint numberOfBonds;
+        mapping(uint => Bond) bonds;
+    }
+
+    mapping (address => CityStruct ) private cities;
+
+    function createCity(string _state, string _county, string _city, string _issuer) public {
+        require(cities[msg.sender].owner == 0);
+        cities[msg.sender] = CityStruct({
+            owner:msg.sender, state:_state, county:_county,
+            city:_city, issuer:_issuer, numberOfBonds:1
+        });
+    }
+
+    function getCity() public view returns(
+        address owner, string _state, string _county,
+        string _city, string _issuer, uint _num) {
+        require(cities[msg.sender].owner != 0);
+        CityStruct storage city = cities[msg.sender];
+        return (city.owner, city.state, city.county, city.city, city.issuer, city.numberOfBonds);
+    }
+
+}
