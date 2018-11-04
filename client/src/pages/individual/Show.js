80ee96a561e9b9b71af71c70cedee8a5aa3029fa
Huy <huy@gohuygo.com>
2018-11-03 21:27:47 -0700

    Clean up the view

--- client/src/pages/individual/Show.js
+++ client/src/pages/individual/Show.js
@@ -17,113 +17,114 @@ class IndividualShow extends Component {
 
   render() {
     return(
-      <Container fluid>
+      <Container>
+        <br />
         <Card.Group>
           <Card
-            header='Elliot Baker'
+            header='City of Berkeley'
             meta='Municipal Bond'
-            description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+            description='1000 Tokens'
             extra={this.renderButtons()}
           />
 
           <Card
-            header='Elliot Baker'
+            header='City of San Jose'
             meta='Municipal Bond'
-            description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+            description='52 Tokens'
             extra={this.renderButtons()}
           />
 
           <Card
-            header='Elliot Baker'
+            header='City of San Francisco'
             meta='Municipal Bond'
-            description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+            description='86 Tokens'
             extra={this.renderButtons()}
           />
 
           <Card
-            header='Elliot Baker'
+            header='City of Napa'
             meta='Municipal Bond'
-            description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+            description='135 Tokens'
             extra={this.renderButtons()}
           />
 
           <Card
-            header='Elliot Baker'
+            header='City of Los Angeles'
             meta='Municipal Bond'
-            description='Elliot is a sound engineer living in Nashville who enjoys playing guitar and hanging with his cat.'
+            description='999 Tokens'
             extra={this.renderButtons()}
           />
         </Card.Group>
-      
 
-      <Divider section />
 
-      <Header as='h2'>Performance Dividends</Header>
-      <Table attached>
-        <Table.Header>
-          <Table.HeaderCell>Bond Name</Table.HeaderCell>
-          <Table.HeaderCell>Dividend Date</Table.HeaderCell>
-          <Table.HeaderCell>Tokens</Table.HeaderCell>
-          <Table.HeaderCell>Withdraw Ether</Table.HeaderCell>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>Fremont School </Table.Cell>
-            <Table.Cell>6/30/2019</Table.Cell>
-            <Table.Cell>100</Table.Cell>
-            <Table.Cell>
-                <a href="https://www.facebook.com/Muniverse"> Withdraw 10 Eth </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-          <Table.Cell>San Francisco Bridge</Table.Cell>
-            <Table.Cell>12/30/2018</Table.Cell>
-            <Table.Cell>30</Table.Cell>
-            <Table.Cell>
-                <a href="https://www.facebook.com/Muniverse"> Withdraw 10 Eth </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-          <Table.Cell>Carolina Parks </Table.Cell>
-            <Table.Cell>3/29/2019</Table.Cell>
-            <Table.Cell>120</Table.Cell>
-            <Table.Cell>0 Eth (alread withdrawn)</Table.Cell>
-          </Table.Row>
-        </Table.Body>
-      </Table>
+        <Divider section />
 
-      <Divider section />
+        <Header as='h2'>Performance Dividends</Header>
+        <Table attached>
+          <Table.Header>
+            <Table.HeaderCell>Bond Name</Table.HeaderCell>
+            <Table.HeaderCell>Dividend Date</Table.HeaderCell>
+            <Table.HeaderCell>Tokens</Table.HeaderCell>
+            <Table.HeaderCell>Withdraw Ether</Table.HeaderCell>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Fremont School </Table.Cell>
+              <Table.Cell>6/30/2019</Table.Cell>
+              <Table.Cell>100</Table.Cell>
+              <Table.Cell>
+                  <a href="https://www.facebook.com/Muniverse"> Withdraw 10 Eth </a>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+            <Table.Cell>San Francisco Bridge</Table.Cell>
+              <Table.Cell>12/30/2018</Table.Cell>
+              <Table.Cell>30</Table.Cell>
+              <Table.Cell>
+                  <a href="https://www.facebook.com/Muniverse"> Withdraw 10 Eth </a>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+            <Table.Cell>Carolina Parks </Table.Cell>
+              <Table.Cell>3/29/2019</Table.Cell>
+              <Table.Cell>120</Table.Cell>
+              <Table.Cell>0 Eth (alread withdrawn)</Table.Cell>
+            </Table.Row>
+          </Table.Body>
+        </Table>
 
-      <Header as='h2'>Mature Bonds</Header>
-      <Table attached>
-        <Table.Header>
-          <Table.HeaderCell>Bond Name</Table.HeaderCell>
-          <Table.HeaderCell>Maturity Date</Table.HeaderCell>
-          <Table.HeaderCell>Tokens</Table.HeaderCell>
-          <Table.HeaderCell>Withdraw Ether</Table.HeaderCell>
-        </Table.Header>
-        <Table.Body>
-          <Table.Row>
-            <Table.Cell>Berkeley Arts center </Table.Cell>
-            <Table.Cell>10/30/2018</Table.Cell>
-            <Table.Cell>23000</Table.Cell>
-            <Table.Cell>
-                <a href="https://www.facebook.com/Muniverse"> Withdraw Eth </a>
-            </Table.Cell>
-          </Table.Row>
-          <Table.Row>
-          <Table.Cell>Fremont Community Hall</Table.Cell>
-            <Table.Cell>11/3/2018</Table.Cell>
-            <Table.Cell>5000</Table.Cell>
-            <Table.Cell>
-                <a href="https://www.facebook.com/Muniverse"> Withdraw Eth </a>
-            </Table.Cell>
-          </Table.Row>
+        <Divider section />
 
-        </Table.Body>
-      </Table>
+        <Header as='h2'>Mature Bonds</Header>
+        <Table attached>
+          <Table.Header>
+            <Table.HeaderCell>Bond Name</Table.HeaderCell>
+            <Table.HeaderCell>Maturity Date</Table.HeaderCell>
+            <Table.HeaderCell>Tokens</Table.HeaderCell>
+            <Table.HeaderCell>Withdraw Ether</Table.HeaderCell>
+          </Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <Table.Cell>Berkeley Arts center </Table.Cell>
+              <Table.Cell>10/30/2018</Table.Cell>
+              <Table.Cell>23000</Table.Cell>
+              <Table.Cell>
+                  <a href="https://www.facebook.com/Muniverse"> Withdraw Eth </a>
+              </Table.Cell>
+            </Table.Row>
+            <Table.Row>
+            <Table.Cell>Fremont Community Hall</Table.Cell>
+              <Table.Cell>11/3/2018</Table.Cell>
+              <Table.Cell>5000</Table.Cell>
+              <Table.Cell>
+                  <a href="https://www.facebook.com/Muniverse"> Withdraw Eth </a>
+              </Table.Cell>
+            </Table.Row>
 
-      <Divider section />
+          </Table.Body>
+        </Table>
+
+        <Divider section />
 
 
 
