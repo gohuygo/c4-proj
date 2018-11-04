3914740834a24946f102aa55c5644a1ade70beeb
Sreenivasulu Lekkala <sreenivasulu.lekkala@gmail.com>
2018-11-03 17:33:02 -0700

    Added crate bond functionality

--- contracts/City.sol
+++ contracts/City.sol
@@ -52,4 +52,52 @@ contract City {
     function isCity() public view returns (bool _isCity) {
         return (cities[msg.sender].owner != 0);
     }
+
+    function createBond(
+        string name,
+        string startDate,
+        string orderDeadline,
+        uint   totalAmountFiat,
+        uint   totalTokensSupply,
+        string tokenSymbol,
+        address bondContractERC20Address,
+        string creditRating,
+        uint   couponRate,
+        uint   faceValue,
+        uint   marketValue,
+        uint   tTMInMonths,
+        string status,
+        string typeOfBond
+    ) public {
+
+        require(cities[msg.sender].owner != 0);
+
+        Bond memory bond;
+        bond.name = name;
+        bond.startDate = startDate;
+        bond.orderDeadline = orderDeadline;
+        bond.totalAmountFiat = totalAmountFiat;
+        bond.totalTokensSupply = totalTokensSupply;
+        bond.tokenSymbol = tokenSymbol;
+        bond.bondContractERC20Address = bondContractERC20Address;
+        bond.creditRating = creditRating;
+        bond.couponRate = couponRate;
+        bond.faceValue = faceValue;
+        bond.marketValue = marketValue;
+        bond.tTMInMonths = tTMInMonths;
+        bond.status = status;
+        bond.typeOfBond = typeOfBond;
+
+        cities[msg.sender].bonds[cities[msg.sender].numberOfBonds] = bond;
+        cities[msg.sender].numberOfBonds++;
+    }
+
+    function getNumberOfBonds() public view returns (uint _num) {
+        return cities[msg.sender].numberOfBonds;
+    }
+
+    function getBond() public view returns (string name, address _addr) {
+        return (cities[msg.sender].bonds[0].name, cities[msg.sender].bonds[0].bondContractERC20Address);
+    }
+
 }
